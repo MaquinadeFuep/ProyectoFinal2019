1cda7cf3c8cf95b0a4e27065ede63d1a6cdbc60a
Vladimir Martinez <rafaelvady.q@gmail.com>
2019-10-29 23:57:39 -0600

    Realizando la subicion

--- BD/Materias.sql
+++ BD/Materias.sql
@@ -7,3 +7,42 @@ horas_semanales int not null,
 tipo_tecnico_general varchar (20) not null
 )
 alter table materias add constraint pk_materia primary key (id_materia)
+
+--Creaciones de procedimiento para agregar
+create procedure spu_agregar_materia
+@id varchar(15),
+@nombremate varchar (30),
+@horas_semanales int,
+@tipo_tecnico_general varchar (20)
+as 
+insert into materias values (@id,@nombremate, @horas_semanales,@tipo_tecnico_general)
+
+exec spu_agregar_materia 'Mate1','Sociales','16','General'
+
+--Creaciones de procedimiento para borrar
+create procedure spu_borrar_materia
+(
+@id varchar (15)
+)
+as
+begin 
+delete from materias where id_materia=@id;
+end
+
+exec spu_borrar_materia 'Mate1'
+
+--Creaciones de procedimiento para modificar- 
+create procedure spu_modificar_nombre
+(
+@id varchar (15),
+@nombremate varchar (30)
+)
+as
+update materias
+set		nombre=@nombremate
+where id_materia=@id
+
+
+exec spu_modificar_nombre ''
+
+
