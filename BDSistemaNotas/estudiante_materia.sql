65f17e453ad8da39a3e043386cf5d7e57562d39e
Vladimir Martinez <rafaelvady.q@gmail.com>
2019-10-30 17:59:10 -0600

    nueva tabla

---
+++ BDSistemaNotas/estudiante_materia.sql
@@ -0,0 +1,11 @@
+use SistemaNotas
+
+create table estudiante_materia
+(
+id_estudiante_materia int identity (0,1) not null,
+id_materia varchar (15) not null,
+anio int not null,
+nie varchar (7) not null,
+);
+
+alter table estudiante_materia add constraint pk_estudiante_materia primary key (id_estudiante_materia)
