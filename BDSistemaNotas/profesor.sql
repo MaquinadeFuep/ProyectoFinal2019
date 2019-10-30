9ed0cb7abb3a8cee1046662a420cd5299a6321ea
Vladimir Martinez <rafaelvady.q@gmail.com>
2019-10-30 16:33:40 -0600

    profesores ver...des

--- BDSistemaNotas/profesor.sql
+++ BDSistemaNotas/profesor.sql
@@ -14,14 +14,19 @@ alter table profesor add constraint pk_profesor primary key (id_profesor)
 alter table profesor add  salario float not null
 alter table profesor add  tipo_contrato varchar (50) not null
 
+alter table profesor alter column salario money  not null
+
 --Procedimiento para agregar profesor
 
 create procedure spu_agregar_profesor
-@id varchar (15)
+@id varchar (15),
+@salario money,
+@tipo_contrato varchar (50)
 as
-insert into profesor (id_profesor) values (@id)
+insert into profesor (id_profesor,salario,tipo_contrato) values (@id,@salario,@tipo_contrato)
 
-exec spu_agregar_profesor ''
+exec spu_agregar_profesor '','',''
+
 
 --Procedimiento para eliminación logica de profesor
 create procedure spu_borradoLogico_profesor
@@ -34,4 +39,17 @@ where id_profesor=@id
 
 exec spu_borradoLogico_profesor ''
  
- --cwhaghdgadjkhkjwhdkjwehdkwjedhwkjdhwk
+ --Procedimiento para modificación de profesor
+
+create procedure spu_modificacion_profesor
+@id varchar (15),
+@salario money,
+@tipo_contrato varchar (50)
+as
+update profesor
+set salario=@salario,
+	tipo_contrato=@tipo_contrato,
+	updated=getdate()
+where id_profesor=@id
+
+exec spu_modificacion_profesor '','',''
