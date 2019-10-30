bfb8efe458262710a911f4298f74766095521e1a
Vladimir Martinez <rafaelvady.q@gmail.com>
2019-10-30 15:04:44 -0600

    estu

--- BDSistemaNotas/estudiantes.sql
+++ BDSistemaNotas/estudiantes.sql
@@ -2,7 +2,7 @@ use SistemaNotas
 
 create table estudiantes 
 (
-nie varchar (20) not null,
+nie varchar (7) not null,
 estado int default (1) not null,
 created datetime default (getdate()) ,
 updated datetime,
@@ -12,20 +12,19 @@ deleted datetime
 alter table estudiantes add constraint pk_estudiantes primary key (nie)
 
 create procedure spu_agregarestudiante
-@id varchar (20)
+@id varchar (7)
 as
 insert into estudiantes (nie) values (@id)
 
-exec spu_agregarestudiante '0097676578'
-
+exec spu_agregarestudiante '0987654'
 
 
 create procedure spu_borrarestudiante 
-@id varchar (20)
+@id varchar (7)
 as
 update estudiantes
 set estado = 0,
 	deleted = getdate()
 where nie = @id
 
-exec spu_borrarmateria ''
+exec spu_borrarestudiante '0987654'
