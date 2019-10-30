b9eda299f5bbbbcceb075b7ace4ac727a04f3cd9
Vladimir Martinez <rafaelvady.q@gmail.com>
2019-10-30 11:38:37 -0600

    con borrados logicos

--- BD/Materias.sql
+++ BD/Materias.sql
@@ -13,11 +13,12 @@ create procedure spu_agregar_materia
 @id varchar(15),
 @nombremate varchar (30),
 @horas_semanales int,
-@tipo_tecnico_general varchar (20)
+@tipo_tecnico_general varchar (20),
+@estado int
 as 
-insert into materias values (@id,@nombremate, @horas_semanales,@tipo_tecnico_general)
+insert into materias values (@id,@nombremate, @horas_semanales,@tipo_tecnico_general,@estado)
 
-exec spu_agregar_materia 'Mate1','Sociales','16','General'
+exec spu_agregar_materia 'Mate1','Sociales','16','General','1'
 
 --Creaciones de procedimiento para borrar
 
@@ -49,3 +50,26 @@ set		nombre=@nombremate,
 where id_materia=@id
 
 exec spu_modificar_materia /* id */ 'Mate1', 'Matelinguistica','69','Tecnico'
+
+alter table materias add estado int 
+
+--Procedimiento para borrado logico de materias
+
+create procedure spu_borradologicomaterias
+@id varchar (15)
+as
+update materias
+set		estado=0		
+where id_materia=@id
+
+exec spu_borradologicomaterias 'Mate1'
+
+--Procedimiento para mostrar datos ocultos logico de materias
+create procedure spu_mostradologicomaterias
+@id varchar (15)
+as
+update materias
+set		estado=1		
+where id_materia=@id
+
+exec spu_mostradologicomaterias 'Mate1'
