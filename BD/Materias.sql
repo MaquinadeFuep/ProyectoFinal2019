2ff9220a2894eb739fd7a06b3a397d9248917b60
Vladimir Martinez <rafaelvady.q@gmail.com>
2019-10-30 11:26:42 -0600

    Todas las modificaciones materias

--- BD/Materias.sql
+++ BD/Materias.sql
@@ -20,6 +20,7 @@ insert into materias values (@id,@nombremate, @horas_semanales,@tipo_tecnico_gen
 exec spu_agregar_materia 'Mate1','Sociales','16','General'
 
 --Creaciones de procedimiento para borrar
+
 create procedure spu_borrar_materia
 (
 @id varchar (15)
@@ -31,46 +32,20 @@ end
 
 exec spu_borrar_materia 'Mate1'
 
---Creaciones de procedimiento para modificar nombre- 
-create procedure spu_modificar_nombre
+--Creaciones de procedimiento para modificar materias- 
+
+create procedure spu_modificar_materia
 (
 @id varchar (15),
-@nombremate varchar (30)
-)
-as
-update materias
-set		nombre=@nombremate
-where id_materia=@id
-
-exec spu_modificar_nombre /* id */ 'Mate1', /* nombre que desea que desea que tenga */'Matelinguistica'
-
---Creaciones de procedimiento para modificar cantidad de horas semanales-- 
-
-create procedure spu_modificar_canthorassemanales
-(
-@id varchar (15),
-@horas_semanales int
-)
-as
-update materias
-set		horas_semanales=@horas_semanales
-where id_materia=@id
-
-exec spu_modificar_canthorassemanales/* id */ 'Mate1', /* nombre que desea que desea que tenga */'100'
-select * from materias
-
-
---Creaciones de procedimiento para modificar cantidad de tipo tecnico o general-- 
-create procedure spu_modificar_tipoTecGene
-(
-@id varchar (15),
+@nombremate varchar (30),
+@horas_semanales int,
 @tipo_tecnico_general varchar (20)
 )
 as
 update materias
-set		tipo_tecnico_general=@tipo_tecnico_general 
+set		nombre=@nombremate,
+		horas_semanales=@horas_semanales,
+		tipo_tecnico_general=@tipo_tecnico_general
 where id_materia=@id
 
-exec spu_modificar_tipoTecGene /* id */ 'Mate1', /* nombre que desea que desea que tenga */'Lass'
-select * from materias
-
+exec spu_modificar_materia /* id */ 'Mate1', 'Matelinguistica','69','Tecnico'
--- BD/creaciondebase.sql
+++ BD/creaciondebase.sql
@@ -17,8 +17,15 @@ genero varchar (25) not null,
 tipo_contrato varchar (50) not null,
 )
 
+alter table usuarios add estado int 
+
 alter table usuarios add constraint pk_username primary key (username)
 
+/* Algunos cambios que crei necesarios xD, el dui que no sea obligatorio porque eso significaria
+que si no tiene dui no se podría meter sus notas, igual el telefono, y el genero que solo ponga M o F */
+alter table usuarios alter column dui varchar (10)
+alter table usuarios alter column telefono varchar (9)
+alter table usuarios alter column genero varchar (1)
 
 -------------------Procedimientos ------------------------------------------------------------
 
@@ -29,18 +36,17 @@ create procedure spu_agregarusuarios
 @nombre varchar (50),
 @apellido varchar (50),
 @fecha_nacimiento date,
-@telefono varchar (10),
-@dui varchar (15),
+@telefono varchar (9),
+@dui varchar (10),
 @direccion varchar (100),
-@genero varchar (25),
-@tipocontrato varchar (50)
+@genero varchar (1),
+@tipocontrato varchar (50),
+@estado int
 as
-insert into usuarios values (@nombreusuario, @contrasenia, @rol, @nombre, @apellido, @fecha_nacimiento, @telefono, @dui, @direccion, @genero, @tipocontrato)
+insert into usuarios values (@nombreusuario, @contrasenia, @rol, @nombre, @apellido, @fecha_nacimiento, @telefono, @dui, @direccion, @genero, @tipocontrato,@estado)
 
-exec spu_agregarusuarios 'Usuario003', '12345', 'Administrador', 'Nume', 'Minu', '12/06/1987', '7873-9839', '00456787-9', 'San Salvador', 'Masculino', 'Por Hora'
-
-
-
+exec spu_agregarusuarios 'Usuario001', '12345', 'Administrador', 'Nume', 'Minu', '12/06/1987', '', '', 'San Salvador', 'F', 'Por Hora','1'
+select * from usuarios
 --Procedimiento para borrar
 
 create procedure spu_borrar_usuario
@@ -52,21 +58,40 @@ begin
 delete from usuarios where username=@id;
 end
 
-exec spu_borrar_usuario 'Usuario001'
+exec spu_borrar_usuario 'Usuario004'
 
+--Procedimiento de actualizar 
 
---Procedimiento de actualizar (ejemplo: de nombre de direccion)
-create procedure spu_modificar
+create procedure spu_modificar_usuario
 (
 @id varchar (15),
-@direccion varchar (100)
+@contrasenia varchar (70),
+@rol varchar (25),
+@nombre varchar (50),
+@apellido varchar (50),
+@fechaNacimiento date,
+@telefono varchar (9),
+@dui varchar (10),
+@direccion varchar (100),
+@genero varchar (1),
+@tipo_contrato varchar (50)
 )
 as
 update usuarios 
-set		direccion=@direccion
+set		
+contrasenia=@contrasenia,
+rol=@rol,
+nombre=@nombre,
+apellido=@apellido,
+fecha_nacimiento=@fechaNacimiento,
+telefono=@telefono,
+dui=@dui,
+direccion=@direccion,
+genero=@genero,
+tipo_contrato=@tipo_contrato
 where username=@id
 
-
-exec spu_modificar 'Usuario003', 'Santa Tecla'
+exec spu_modificar_usuario 'Usuario001', '123pandahermoso','Estudiante','Vlady','Martinez','12/02/2000','2310-3962','09876543-1','San Juan Opico','M','Mined'
 
 select * from usuarios
+
