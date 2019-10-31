709f0339d5aadac60c80c4719d7db3c40b2d6e73
Vladimir Martinez <rafaelvady.q@gmail.com>
2019-10-30 18:04:51 -0600

    solo falta notas (lo mas perro) y relaciones

---
+++ BDSistemaNotas/profesor_materia.sql
@@ -0,0 +1,10 @@
+use SistemaNotas
+
+create table profesor_materia
+(
+id_profesor_materia int identity (0,1) not null,
+id_profesor varchar (15) not null,
+id_materia varchar (15) not null,
+);
+
+alter table profesor_materia add constraint pk_id_profesor_materia primary key (id_profesor_materia)
