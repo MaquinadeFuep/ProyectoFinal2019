74a7f98902bbb6bc234aba4a9814db1558f0f646
Vladimir Martinez <rafaelvady.q@gmail.com>
2019-10-30 19:21:19 -0600

    las foraneas men

---
+++ BDSistemaNotas/foreaneas.sql
@@ -0,0 +1,6 @@
+use SistemaNotas
+
+alter table estudiante_materia add constraint fk_estudiante_materia foreign key (id_materia) references materias (id_materia)
+alter table estudiante_materia add constraint fk_estudiante_materia1 foreign key (nie) references estudiantes (nie)
+alter table profesor_materia add constraint fk_profesor_materia foreign key (id_profesor) references profesor (id_profesor)
+alter table profesor_materia add constraint fk_profesor_materia1 foreign key (id_materia) references materias (id_materia)
