810dcf7bdd75e47c48430a47907efd0937929189
David Argumedo <dargumedo15@gmail.com>
2019-11-01 00:28:35 -0600

    la carpeta prrona de visual 1.0

---
+++ Notas_Bachillerato/Notas_Bachillerato/Clases/Transacciones.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Notas_Bachillerato.Clases
+{
+    class Transacciones
+    {
+        private string sql;
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private Usuarios user;
+
+        public Transacciones()
+        {
+            this.sql = "";
+            this.con = new SqlConnection();
+            this.cmd = new SqlCommand();
+            this.con.ConnectionString = "Data Source=HP-PC\SQLEXPRESS; Initial Catalog=DBNotas_Bachillerato; Integrated Security=True";
+        }
+
+        public DataTable consultar(string tabla)
+        {
+            DataTable datos = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            switch (tabla)
+            {
+                case "Usuario":
+                    this.sql = "select * from Usuario";
+                    break;
+                case "otra tabla":
+                    this.sql = "select a otra tabla";
+                    break;
+                default:
+                    MessageBox.Show("Tabla " + tabla + " no existe!");
+                    break;
+            }
+
+            try
+            {
+                this.con.Open();
+                adapter = new SqlDataAdapter(this.sql, this.con.ConnectionString);
+                adapter.Fill(datos);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocurrio este error: " + ex.Message);
+            }
+            finally
+            {
+                this.con.Close();
+            }
+            return datos;
+        }
+
+        public bool ejecutar(string sql)
+        {
+            try
+            {
+                this.cmd.CommandText = sql;
+                this.cmd.Connection = this.con;
+                this.cmd.Connection.Open();
+                this.cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                this.cmd.Connection.Close();
+            }
+        }
+
+        public bool insertar(object obj, string tabla)
+        {
+            switch (tabla)
+            {
+                case "Usuario":
+                    this.user = (Usuarios)obj;
+                    this.sql = "insert into Usuario values ('" + this.user.NombreUsuario + "'," + this.user.IdDocente + ",'" + this.user.Contra + "'," + this.user.IdTipo + ")";
+                break;
+                case "otra tabla":
+                break;
+                default:
+                    MessageBox.Show("Tabla inválida");
+                break;
+            }
+            if (ejecutar(this.sql))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool modificar(object obj, string tabla)
+        {
+            switch (tabla)
+            {
+                case "Usuario":
+                    this.user = (Usuarios)obj;
+                    this.sql = "update Usuario set IdDocente=" + this.user.IdDocente + ", Contraseña='" + this.user.Contra + "', IdTipo=" + this.user.IdTipo + " where Nombre_usuario='" + this.user.NombreUsuario + "'";
+                break;
+                case "otra tabla":
+                break;
+                default:
+                    MessageBox.Show("Tabla inválida");
+                break;
+            }
+            if (ejecutar(this.sql))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool eliminar(object obj, string tabla)
+        {
+            switch (tabla)
+            {
+                case "Usuario":
+                    this.user = (Usuarios)obj;
+                    this.sql = "delete Usuario where Nombre_usuario= '"+this.user.NombreUsuario+"'";
+                break;
+                case "otra tabla":
+                break;
+                default:
+                    MessageBox.Show("Tabla inválida");
+                break;
+            }
+            if (ejecutar(this.sql))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public DataTable filtrar(string tabla, string campo, string criterio)
+        {
+            DataTable datos = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            this.sql = "select * from " + tabla + " where " + campo + " like '%" + criterio + "%'";
+
+            try
+            {
+                this.con.Open();
+                adapter = new SqlDataAdapter(this.sql, this.con.ConnectionString);
+                adapter.Fill(datos);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocurrio este error: " + ex.Message);
+            }
+            finally
+            {
+                this.con.Close();
+            }
+            return datos;
+        }
+    }
+}
---
+++ Notas_Bachillerato/Notas_Bachillerato/Formularios/frmUsuarios.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Notas_Bachillerato.Clases;
+
+namespace Notas_Bachillerato.Formularios
+{
+    public partial class frmUsuarios : Form
+    {
+
+        Transacciones t = new Transacciones();
+        Usuarios user = new Usuarios();
+
+        public void cargar()
+        {
+            this.gridDatos.DataSource = null;
+            this.gridDatos.DataSource = t.consultar("Usuario");
+        }
+
+        public frmUsuarios()
+        {
+            InitializeComponent();
+            cargar();
+        }
+
+        private void setiar()
+        {
+            user.NombreUsuario = this.txtNombUsuario.Text;
+            user.IdDocente = int.Parse(this.txtIdDocente.Text);
+            user.Contra = this.txtContra.Text;
+            user.IdTipo = int.Parse(this.cmbTipoUsuario.Text);
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            setiar();
+
+            if (t.insertar(user, "Usuario"))
+            {
+                MessageBox.Show("Usuario agregado exitosamente");
+                cargar();
+            }
+            else
+            {
+                MessageBox.Show("¡Error!");
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            setiar();
+
+            if (t.modificar(user, "Usuario"))
+            {
+                MessageBox.Show("Usuario modificado exitosamente");
+                cargar();
+            }
+            else
+            {
+                MessageBox.Show("¡Error!");
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            setiar();
+
+            if (MessageBox.Show(this, "¿Está seguro de eliminar esto?", "Eliminar registro", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                if (t.eliminar(this.user, "Usuario"))
+                {
+                    MessageBox.Show("Usuario eliminado exitosamente");
+                    cargar();
+                }
+                else
+                {
+                    MessageBox.Show("No se borró el usuario seleccionado");
+                }
+            }
+        }
+
+        private void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            gridDatos.DataSource = null;
+            gridDatos.DataSource = t.filtrar("Usuario", cmbCampos.Text, txtCriterio.Text);
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            cargar();
+            this.txtCriterio.Clear();
+            cmbCampos.Text = null;
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
