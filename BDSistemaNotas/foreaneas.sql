0ef52de1cba82dce14731ab5848624cc0315b7ca
David Argumedo <dargumedo15@gmail.com>
2019-10-31 23:02:29 -0600

    buassd

--- BDSistemaNotas/foreaneas.sql
+++ BDSistemaNotas/foreaneas.sql
@@ -5,10 +5,10 @@ alter table alumnos add constraint fk_grado foreign key (id_grado) references gr
 alter table estudiante_materia add constraint fk_alumno foreign key (id_alumno) references alumnos (nie)
 alter table estudiante_materia add constraint fk_materia foreign key (id_materia) references materia (id_materia)
 
-alter table nota add constraint fk_ foreign key (id_materia) references materia (id_materia)
+alter table nota add constraint fk_materia1 foreign key (id_materia) references materia (id_materia)
 
 alter table profesor_materia add constraint fk_profesor foreign key (id_profesor) references profesor (escalafon)
-alter table profesor_materia add constraint fk_materia1 foreign key (id_materia1) references materia (id_materia)
+alter table profesor_materia add constraint fk_materia2 foreign key (id_materia1) references materia (id_materia)
 
 
 
