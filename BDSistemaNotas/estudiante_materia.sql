06f722183222f1796c76b014b689cd018f3b47aa
Vladimir Martinez <rafaelvady.q@gmail.com>
2019-10-31 22:27:40 -0600

    LLAVES PRIMARIAS Y OTRA ONDAS

--- BDSistemaNotas/estudiante_materia.sql
+++ BDSistemaNotas/estudiante_materia.sql
@@ -4,8 +4,9 @@ create table estudiante_materia
 (
 id_estudiante_materia int identity (0,1) not null,
 id_materia varchar (15) not null,
-anio int not null,
+anio int,
 nie varchar (7) not null,
 )
 
---aqui no le agregue clave porque no se si llevan 
+alter table estudiante_materia add constraint pk_estudiante_materia primary key (id_estudiante_materia)
+--NOTA, LLAVES PRIMARIA AGREGADA, NIE AGREGADO NOT NULL
